b9e3a922e9715f130583a66dcd6288faa93eba76
fazebian <rabariari.sa@gmail.com>
2023-10-02 09:04:44 +0300

    Initial commit

---
+++ src/main/java/xyz/sahia/zazabanki/ZazaBankiApplication.java
@@ -0,0 +1,13 @@
+package xyz.sahia.zazabanki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZazaBankiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZazaBankiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/sahia/zazabanki/ZazaBankiApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.sahia.zazabanki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZazaBankiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
